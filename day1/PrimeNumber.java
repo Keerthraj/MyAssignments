21e2ba9de1e955d426eeefc75ad84e57cd6d89ed
Keerthraj <127030766+Keerthraj@users.noreply.github.com>
2023-03-11 16:29:25 +0530

    Add files via upload

--- day1/PrimeNumber.java
+++ day1/PrimeNumber.java
@@ -3,8 +3,18 @@ package week.day1;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 131;
+		boolean bprime = true;
+		for(int i = 2; i < input ; i++) {
+			if (input%i==0) {
+				System.out.println("Non Prime");
+				bprime = false;
+				break;
+			}
+		}
+		if (bprime) {
+			System.out.println("Prime Number");
+		}
 	}
 
 }
